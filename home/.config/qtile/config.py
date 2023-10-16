fe333c2887b84a703b70893ac33d479e8cda6d94
Sahir Maredia <sahirzm@gmail.com>
2023-10-15 22:08:56 -0400

    - Remove vscodium from installed packges - Dired enhancements - replace vi keys with tmux

--- home/.config/qtile/config.py
+++ home/.config/qtile/config.py
@@ -49,7 +49,7 @@ keys = [
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    Key([mod], "space", lazy.next_screen(), desc="Move window focus to other window"),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
@@ -430,7 +430,8 @@ floating_layout = layout.Floating(
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
-
+cursor_wrap = True
+follow_mouse_focus = True
 # If things like steam games want to auto-minimize themselves when losing
 # focus, should we respect this or not?
 auto_minimize = True
