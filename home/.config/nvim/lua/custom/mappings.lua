4b97e22bd55e50dc282fa3a3ce6b153e19e569dd
Sahir Maredia <sahirzm@gmail.com>
2024-01-02 12:39:28 -0500

    Fix nvim-tmux-navigation configuration

--- home/.config/nvim/lua/custom/mappings.lua
+++ home/.config/nvim/lua/custom/mappings.lua
@@ -4,6 +4,11 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<cr>", "window left" },
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<cr>", "window down"},
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<cr>", "window up" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<cr>", "window right" },
+    ["<C-\\>"] = { "<cmd> TmuxNavigatePrevious<cr>", "previous window" },
   },
   v = {
     [">"] = { ">gv", "indent"},
