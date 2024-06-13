81d19b6ca2843c6f3511e18eec804be8430d8deb
Sahir Maredia <sahirzm@gmail.com>
2024-06-13 00:05:38 -0600

    Update NvChad configuration

---
+++ home/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = 'BufWritePre',
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+  	"williamboman/mason.nvim",
+  	opts = require("configs.mason")
+  },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = require("configs.treesitter")
+  },
+}
