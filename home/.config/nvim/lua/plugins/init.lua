19d40257ec7ef8a17da825fb263da7424f6dc639
Sahir Maredia <sahirzm@gmail.com>
2024-06-18 11:44:48 -0600

    Add nvim-surround and repeat to neovim

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -22,7 +22,20 @@ return {
   	opts = require("configs.treesitter")
   },
   {
+    "tpope/vim-repeat"
+  },
+  {
     "ggandor/leap.nvim",
     lazy = false
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
   }
 }
