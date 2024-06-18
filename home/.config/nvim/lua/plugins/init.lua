0ae4b9cf2165d466fe7a2db59680a13adff1ec53
Sahir Maredia <sahirzm@gmail.com>
2024-06-18 11:57:52 -0600

    Add support to autoclose tags in neovim

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -37,5 +37,18 @@ return {
             -- Configuration here, or leave empty to use defaults
         })
     end
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("nvim-ts-autotag").setup({
+        opts = {
+          enable_close = true,
+          enable_rename = true,
+          enable_close_on_slash = false
+        }
+      })
+    end
   }
 }
