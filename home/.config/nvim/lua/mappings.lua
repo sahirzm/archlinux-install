85b9c882145540683877dc32042696e120d53849
Sahir Maredia <sahirzm@gmail.com>
2024-06-18 12:33:19 -0600

    Add csv editing support for neovim

--- home/.config/nvim/lua/mappings.lua
+++ home/.config/nvim/lua/mappings.lua
@@ -5,13 +5,13 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("v", ">", ">gv", { desc = "indent"})
+map("v", ">", ">gv", { desc = "indent" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
 -- keybindings for motion - leap.nvim
-map({"n", "x", "o"}, "s", "<Plug>(leap-forward)")
-map({"n", "x", "o"}, "S", "<Plug>(leap-backward)")
+map({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
+map({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
 
 -- ranger
 map("n", "<leader>ef", "", {
--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -60,4 +60,42 @@ return {
       }
     end,
   },
+  {
+    "emmanueltouzery/decisive.nvim",
+    config = function()
+      require("decisive").setup {}
+    end,
+    lazy = true,
+    ft = { "csv" },
+    keys = {
+      {
+        "<leader>cca",
+        ":lua require('decisive').align_csv({})<cr>",
+        { silent = true },
+        desc = "Align CSV",
+        mode = "n",
+      },
+      {
+        "<leader>ccA",
+        ":lua require('decisive').align_csv_clear({})<cr>",
+        { silent = true },
+        desc = "Align CSV clear",
+        mode = "n",
+      },
+      {
+        "[c",
+        ":lua require('decisive').align_csv_prev_col()<cr>",
+        { silent = true },
+        desc = "Align CSV prev col",
+        mode = "n",
+      },
+      {
+        "]c",
+        ":lua require('decisive').align_csv_next_col()<cr>",
+        { silent = true },
+        desc = "Align CSV next col",
+        mode = "n",
+      },
+    },
+  },
 }
