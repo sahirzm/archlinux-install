735f3d1c2010a2109d614958fdebf599afe80243
Sahir Maredia <sahirzm@gmail.com>
2025-02-04 07:45:52 -0500

    configs for wezterm and awesome wm

---
+++ home/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,6 @@
+return {
+  "williamboman/mason-lspconfig.nvim",
+  config = {
+    ensure_installed = { "lua_ls" },
+  },
+}
---
+++ home/.config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require("wezterm")
+
+local config = {}
+config.color_scheme = "Tokyo Night Moon"
+
+config.window_decorations = "RESIZE"
+config.use_fancy_tab_bar = false
+
+return config
