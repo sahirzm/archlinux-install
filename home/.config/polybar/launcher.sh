d1ca4ec0f05f6e0c57dfefb3048b8d90707cdcb2
Sahir Maredia <sahirzm@gmail.com>
2025-02-04 22:44:16 -0500

    centralize dpi config for i3 and polybar

--- home/.config/polybar/launcher.sh
+++ home/.config/polybar/launcher.sh
@@ -8,5 +8,5 @@ DIR="$HOME/.config/polybar"
 
 # Launch the bar
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-  MONITOR=$m polybar -q --reload main -c "$DIR"/config.ini &
+  MONITOR=$m DPI=${1:-192} polybar -q --reload main -c "$DIR"/config.ini &
 done
