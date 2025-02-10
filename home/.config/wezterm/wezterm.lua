a49bb1151b446017fc9bb7aed78db4c215b7b182
Sahir Maredia <sahirzm@gmail.com>
2025-02-09 23:07:17 -0500

    Autostart wezterm terminal for i3

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -3,6 +3,20 @@ local wezterm = require("wezterm")
 local config = {}
 config.color_scheme = "Tokyo Night Moon"
 
+config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 3000 }
+config.keys = {
+	{
+		key = "|",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "-",
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+}
+
 config.window_decorations = "RESIZE"
 config.use_fancy_tab_bar = false
 
