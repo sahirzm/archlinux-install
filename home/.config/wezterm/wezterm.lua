bca9d2c45efe4bba07b701a9e97a795c242611d9
Sahir Maredia <sahirzm@gmail.com>
2025-03-05 16:37:54 -0500

    Add binding for pane movememnt for wezterm

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -15,6 +15,31 @@ config.keys = {
 		mods = "LEADER",
 		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
 	},
+	{
+		key = "h",
+		mods = "LEADER",
+		action = wezterm.action({ ActivatePaneDirection = "Left" }),
+	},
+	{
+		key = "j",
+		mods = "LEADER",
+		action = wezterm.action({ ActivatePaneDirection = "Down" }),
+	},
+	{
+		key = "k",
+		mods = "LEADER",
+		action = wezterm.action({ ActivatePaneDirection = "Up" }),
+	},
+	{
+		key = "l",
+		mods = "LEADER",
+		action = wezterm.action({ ActivatePaneDirection = "Right" }),
+	},
+	{
+		key = "z",
+		mods = "LEADER",
+		action = "TogglePaneZoomState",
+	},
 }
 
 config.window_decorations = "RESIZE"
