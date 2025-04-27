34adf4aca957b5329c18f69bc14edb808f15bb9e
Sahir Maredia <sahirzm@gmail.com>
2025-04-26 23:26:09 -0400

    New wallpapers. Switch terminal to kitty

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -4,7 +4,7 @@
 
 if vim.g.neovide then
   vim.o.guifont = "FiraCode Nerd Font Mono:h10"
-  vim.g.neovide_transparency = 0.95
+  vim.g.neovide_opacity = 0.95
   vim.g.neovide_cursor_animation_length = 0.06
   vim.g.neovide_scroll_animation_length = 0
 end
