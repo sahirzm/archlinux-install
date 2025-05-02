4d50692ff479652784b2364cef315a410901a625
Sahir Maredia <sahirzm@gmail.com>
2025-05-02 12:53:52 -0400

    More updates
    - Fix socket file for xidlehook
    - lazyvim enabled biome
    - removed auto-cpufreq and added gnome-keyring

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -8,3 +8,6 @@ if vim.g.neovide then
   vim.g.neovide_cursor_animation_length = 0.06
   vim.g.neovide_scroll_animation_length = 0
 end
+
+-- Enable this option to avoid conflicts with Prettier.
+vim.g.lazyvim_prettier_needs_config = true
