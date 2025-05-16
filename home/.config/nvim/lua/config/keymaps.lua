0e93977bec1d9af483df3943c267dff8b3387e1b
Sahir Maredia <sahirm@amazon.com>
2025-05-16 10:14:50 -0400

    add CsvView plugin for neovim

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
+local csvview = require("plugins.csvview")
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+-- toggle csv view
+map("n", "<leader>cvt", "<cmd>CsvViewToggle<cr>", { desc = "Toggle CsvView" })
