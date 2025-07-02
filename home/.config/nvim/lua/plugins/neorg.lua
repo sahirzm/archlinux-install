190d2ef738616d7e0353b9a3458bf5b1a00fca49
Sahir Maredia <sahirzm@gmail.com>
2025-07-01 23:55:45 -0400

    more minor changes

--- home/.config/nvim/lua/plugins/neorg.lua
+++ home/.config/nvim/lua/plugins/neorg.lua
@@ -1,6 +1,41 @@
 return {
   "nvim-neorg/neorg",
+  dependencies = { "luarocks.nvim" },
   lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
   version = "*", -- Pin Neorg to the latest stable release
-  config = true,
+  config = function()
+    require("neorg").setup({
+      load = {
+        ["core.defaults"] = {},
+        ["core.concealer"] = {},
+        ["core.completion"] = {
+          config = {
+            engine = "nvim-cmp",
+            sources = {
+              { name = "words" },
+              { name = "snippets" },
+              { name = "emoji" },
+            },
+          },
+        },
+        ["core.presenter"] = { config = { zen_mode = "zen-mode" } },
+        ["core.export"] = {},
+        ["core.export.markdown"] = { config = { extensions = "all" } },
+        ["core.summary"] = {},
+        ["core.tangle"] = {},
+        ["core.looking-glass"] = {},
+        ["core.qol.toc"] = {},
+        ["core.qol.todo_items"] = {},
+        ["core.dirman"] = {
+          config = {
+            workspaces = {
+              work = "~/notes/work",
+              notes = "~/notes/notes",
+            },
+            default_workspace = "notes",
+          },
+        },
+      },
+    })
+  end,
 }
